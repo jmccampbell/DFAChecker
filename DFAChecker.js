1b6da4f1172aae1df0a08618c14d23ecbbf70236
jmccampbell <jmccampbell@smith.edu>
2017-04-29 16:00:27 -0400

    added comments, made directions more clear

--- DFAChecker.js
+++ DFAChecker.js
@@ -1,55 +1,75 @@
+/*
+Jenna McCampbell
+DFA Checker
+Spring 2017
+*/
+
+//keeps track of current user mode
 var currentCase = "none";
-var canvas = document.getElementById("canvas");
+/* x1, x2, y1, y2 keep track of point click coordinates
+that must be kept from one event handler to another*/
 var x1;				//several of these are Very Bad Global Variables
-var x2;				//I have made peace with the Code Structure Gods
-var y1;				//and come to terms with my sins
-var y2;
+var x2;				//I could make a couple objects to avoid them, but my code works just fine, so
+var y1;				//I have made peace with the Code Structure Gods
+var y2;				//and come to terms with my sins
+//keeps track of first state selected in arc drawing processes
 var downTarget;
+//keeps track of number of states for element Id-ing purposes
 var stateIndex=0;
+//keeps track of start state of DFA, that with content "0"
 var startState;
+//keeps track of arcs associated with each state
 var stateArcs={};
+//keeps track of which state(s) are excepting
 var accepting=[];
 
+//changes case to allow for adding arcs, displays corresponding instructional text
 function setAddArc(){
 	currentCase = "AddArc";
 	var message = document.getElementById("message");
 	message.textContent = "Click and drag from one state to another to add an arc"
 }
 
+//changes case to allow for adding states, displays corresponding instructional text
 function setAddState() {
 	currentCase = "Add";
 	var message = document.getElementById("message");
-	message.textContent = "Click anywhere to add a state to your DFA";
+	message.textContent = "Click anywhere to add a state to your DFA. Start state must have content \"0\" (zero)";
 }
 
+//changes case to allow for removing states, displays corresponding instructional text
 function setRemove() {
 	currentCase = "Remove";
 	var message = document.getElementById("message");
 	message.textContent = "Click on a state to remove it from your DFA";
 }
 
+//changes case to allow for removing arcs, displays corresponding instructional text
 function setRemoveArcs() {
 	currentCase = "RemoveArcs";
 	var message = document.getElementById("message");
 	message.textContent = "Click and drag from one state to another to remove arcs form your DFA";
 }
 
+//changes case to allow for making states accepting, displays corresponding instructional text
 function setAcceptState() {
 	currentCase = "Accept";
 	var message = document.getElementById("message");
 	message.textContent = "Click on state(s) to make them accepting";
 }
 
+//changes case to allow for inputing string, displays corresponding instructional text
 function setInput(){
 	currentCase = "Input";
 	var message = document.getElementById("message");
 	message.textContent = "Type string and click submit to evaluate";
 }
 
+//gets user input, displays string, calls runString to begin animation
 function getInput(){
-	var clear = document.getElementById("stringText");
-	if (clear != undefined){
-		clear.remove();
+	var clear = document.getElementById("stringText"); 
+	if (clear != undefined){	
+		clear.remove();			//get rid of old string/accepting or rejecting decision
 	}
 	var input = document.getElementById("input").value;
 	var characters = input.split("");
@@ -68,6 +88,8 @@ function getInput(){
 	arrow.textContent = "-->"
 	g.appendChild(arrow);
 	x += 30;
+	/*create separate svg text element for each character of string
+	to allow them to be handled separately*/
 	for (var i = 0; i < characters.length; i++){
 		var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
 		text.setAttribute("id", "text"+id);
@@ -85,6 +107,7 @@ function getInput(){
 	runString(characters);
 }
 
+//controller function that steps through animation of string passing through DFA
 function runString(characters){
 		var currG = startState;
 		if (currG == undefined){
@@ -92,12 +115,13 @@ function runString(characters){
 			return;
 		}
 		var currState = currG.childNodes.item(0);
+		//light up start state and text arrow to indicate beginning of string animation
 		var arrow = document.getElementById("arrow");
 		var time = 0;
 		lightUp(currState, arrow, 0,  time);
-		time += 2000;
+		time += 1500;
 		unLight(currState, arrow, 0,  time);
-        time += 2000;
+        time += 1500;
         var next = findNextState(currState.parentNode.getAttribute("id"), characters[0]);
         if (next == undefined){
         	evaluate(-1, time);
@@ -107,11 +131,12 @@ function runString(characters){
         var currArc = next[1].parentNode.childNodes.item(0);
         nextState = nextState.childNodes.item(0);
         currState = nextState;
+    //light up each arc, next state of DFA, and corresponding character
 	for (var i = 1; i <= characters.length; i++){
 		var stateId = currState.parentNode.getAttribute("id");
 		currChar = document.getElementById("text"+(i-1));
 		lightUp(currState, currChar, currArc, time);
-		time += 2000;
+		time += 1500;
 		unLight(currState, currChar, currArc, time);
         next = findNextState(stateId, characters[i]);
 		if (next != undefined){
@@ -119,19 +144,23 @@ function runString(characters){
         	var nextArc = next[1].parentNode.childNodes.item(0);
 			nextState = nextState.childNodes.item(0);
         	currState = nextState;
+        /* dynamically checks for DFA validity; if a string contains
+        a character with no corresponding arc leading from current state,
+        alert user*/
         } else if (next == undefined && i < characters.length) {
         	setTimeout(function() {
-				alert("Invalid DFA");
+				alert("Invalid DFA or string");
 			}, time);
 			return;
         }
         currArc = nextArc;
-        time += 2000;
+        time += 1500;
 	}
-	evaluate(currState.parentNode.getAttribute("id"), time-2000);
+	//check if string is accepted or regected
+	evaluate(currState.parentNode.getAttribute("id"), time-1500);
 }
 
-
+//checks if string ends in accept state, displays "accepted" or "rejected" message
 function evaluate(currState, time) {
 	var isAccept = (accepting.indexOf(currState) != -1)
 	var stringText = document.getElementById("stringText");
@@ -153,6 +182,7 @@ function evaluate(currState, time) {
 	}, time);
 }
 
+//lights up given state, character, and arc, at given time
 function lightUp(stateToLight, charToLight, arcToLight, time){
 	setTimeout(function () {
 			charToLight.setAttribute("fill", "#FF3B3F");
@@ -164,6 +194,7 @@ function lightUp(stateToLight, charToLight, arcToLight, time){
 		}, time);
 }
 
+//unlights given state, character, and arc at given time
 function unLight(stateToUnLight, charToUnLight, arcToUnLight, time){
 	setTimeout(function () {
                charToUnLight.setAttribute("fill", "#A9A9A9");
@@ -175,28 +206,25 @@ function unLight(stateToUnLight, charToUnLight, arcToUnLight, time){
             }, time);
 }
 
+/*given current state and next character of string,
+returns two element array of next state, and arc taken to get there*/ 
 function findNextState(currState, nextChar){
 	var arcs = stateArcs[currState];
 	var arcOptions = [];
 	var result;
 	for (var i = 0; i < arcs.length; i++){
 		var arcStates = arcs[i].split("");
-		console.log("arcStates: ", arcStates);
 		if (arcStates[0] == currState){
-			console.log("pushed: ",arcs[i]);
 			arcOptions.push(arcs[i]);
 		}
 	}
 	for (i = 0; i < arcOptions.length; i++){
-		console.log("currentOption: ", arcOptions[i]);
 		var currStates = arcOptions[i].split("");
 		var parent = document.getElementById(arcOptions[i]);
 		var currArc = parent.childNodes.item(1);
 		var arcId = currArc.textContent.split(/\s*,\s*/);
 		for( var j = 0; j < arcId.length; j++){
-			console.log("arcId: ", arcId," nextChar: ", nextChar);
 			if (arcId[j] == nextChar){
-				console.log("yay")
 				var resultState = document.getElementById(currStates[1]);
 				result = [resultState, currArc];
 			}
@@ -207,23 +235,28 @@ function findNextState(currState, nextChar){
 	
 }
 
+//handles click event depending on current user mode
 function clickHandler(e) {
 	switch(currentCase) {
 	
 	case "Add":
+		//draws state at user click coordinates
 		draw(e);
 	break;
 	
 	case "Remove":
 		if (document.addEventListener ){
     		document.addEventListener("click", function(event){});
+    			//find element under click
         		var targetElement = event.target || event.srcElement;
+        		//remove state that was clicked, and associated arcs
         		if (targetElement.tagName == "circle" || targetElement.tagName == "text"){
         			var stateId = targetElement.parentNode.getAttribute("id");
-        			var thisArcs = stateArcs[stateId];
+        			var thisArcs = stateArcs[stateId];		//arcs of state to remove
+        			/*for each arc of state to remove, remove arc from every state's list of arcs
+        			on which it appears, remove arc from document*/
         			for (var i = 0; i < thisArcs.length; i++) {
         				var arcId = thisArcs[i];
-        				console.log("arcId: ", arcId);
         				//states
         				var toRemove = document.getElementById(arcId);
         				toRemove.remove();
@@ -241,9 +274,11 @@ function clickHandler(e) {
         			}
         			var n = parseInt(stateId);
         			delete stateArcs[n];
+        			//update startState if current startState is removed
         			if(targetElement.parentNode.childNodes.item(1).textContent == "0"){
         				startState = undefined;
         			}
+        			//update list of accepting states, if accepting state is removed
         			var acceptIndex = accepting.indexOf(stateId);
         			if (acceptIndex > -1){
         				accepting.splice(acceptIndex, 1);
@@ -251,20 +286,23 @@ function clickHandler(e) {
         			targetElement.parentNode.remove();
         			
         		}
-    		
-		} else if (document.attachEvent) {    
+    	}
+    	/* format for firefox?	
+		 else if (document.attachEvent) {    
     		document.attachEvent("onclick", function(){
         		var targetElement = event.target || event.srcElement;
         		if (targetElement.tagName == "circle" || targetElement.tagName == "text"){
         			targetElement.parentNode.remove();
         		}
     		});
-		}
+		}*/
 		break;
 		
 	case "Accept":
         	var targetElement = event.target || event.srcElement;
         	if ((targetElement.tagName == "circle" || targetElement.tagName == "text")){
+        		/*if click state is not already an accepting state, add ring in document
+        		to indicate accepting status, add state to accepting states list*/
         		if (targetElement.parentNode.childNodes.length < 4){
         			var g = targetElement.parentNode;
         			var cx = targetElement.getAttribute("cx");
@@ -277,7 +315,6 @@ function clickHandler(e) {
 					circle.setAttribute("stroke", "#A9A9A9");
 					g.insertBefore(circle, g.childNodes.item(1));
 					accepting.push(g.getAttribute("id"));
-					console.log("accepting: ",accepting);
 				}
         	}
 
@@ -286,90 +323,60 @@ function clickHandler(e) {
 	}
 }
 
+//handles mouse down event depending on current user mode
 function mouseDownHandler(e) {
 	switch(currentCase) {
 		case "AddArc":
 			if (document.addEventListener ){
     			document.addEventListener("mousedown", function(event){});
-        	var targetElement = event.target || event.srcElement;
-        	downTarget = targetElement;
-        	if (targetElement.tagName == "circle" || targetElement.tagName == "text"){
-        			/*var circle= targetElement.parentNode.getElementsByTagName("circle")[0];
-        			x1 = circle.getAttribute("cx");
-        			y1 = circle.getAttribute("cy");*/
-        		var pos = getMousePos(canvas, e);
-    			x1 = pos.x;
-    			y1 = pos.y;
-        	}
-    		
-		} /*else if (document.attachEvent) { 
-    		document.attachEvent("mousedown", function(){
         		var targetElement = event.target || event.srcElement;
+        		//store clicked state to be used in mouseUpHandler
         		downTarget = targetElement;
         		if (targetElement.tagName == "circle" || targetElement.tagName == "text"){
-        			/*var circle= targetElement.parentNode.getElementsByTagName("circle")[0];
-        			x1 = circle.getAttribute("cx");
-        			y1 = circle.getAttribute("cy");
+        			//store current click coordinates to be used in mouseUpHandler
         			var pos = getMousePos(canvas, e);
-    					x1 = pos.x;
-    					y1 = pos.y;
+    				x1 = pos.x;
+    				y1 = pos.y;
         		}
-    		});
-		}*/
+    		}	
 		break;
 		
 		case "RemoveArcs":
 			var targetElement = event.target || event.srcElement;
+			//store clicked state to be used in mouseUpHandler
 			downTarget = targetElement;
 	}
 }
 
 function mouseUpHandler(e) {
+	//get coordinates of mouse up event
 	var pos = getMousePos(canvas, e);
 	switch(currentCase) {
 		case "AddArc":
 				var targetElement = e.target || e.srcElement;
+				//add arc only if mouse up occurs in the circle element of a DFA state
         		if (targetElement.tagName == "circle"){
-        			var circle= targetElement.parentNode.getElementsByTagName("circle")[0];
-        			var cx = parseInt(circle.getAttribute("cx"));
-        			var cy = parseInt(circle.getAttribute("cy"));
+					//get coordinates of mouse up event
         			var pos = getMousePos(canvas, event);
-    					x2 = pos.x;
-    					y2 = pos.y;
+    				x2 = pos.x;
+    				y2 = pos.y;
+    				//if arc is from a state to itself, draw self-referential arc
     				if (targetElement.isSameNode(downTarget)){
+    					var circle= targetElement.parentNode.getElementsByTagName("circle")[0];
+        				var cx = parseInt(circle.getAttribute("cx"));
+        				var cy = parseInt(circle.getAttribute("cy"));
     					drawArc(cx,cy, targetElement);
+    				//else draw arc as a line
     				} else {
         				drawLine(x1, y1, x2, y2, targetElement);
         			}
         			
         		}
-		/*else if (document.attachEvent) {    
-			console.log("yo");
-    		document.attachEvent("mouseup", function(){
-        		var targetElement = event.target || event.srcElement;
-        		if (targetElement.tagName == "circle" || targetElement.tagName == "text"){
-        			/*var circle= targetElement.parentNode.getElementsByTagName("circle")[0];
-        			x2 = circle.getAttribute("cx");
-        			y2 = circle.getAttribute("cy");
-        			var pos = getMousePos(canvas, event);
-    					x2 = pos.x;
-    					y2 = pos.y;
-        			var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-        			console.log("line: x1:"+x1+", y1:"+y1+" x2:"+x2+" y2:"+y2);
-        			line.setAttribute("x1", x1);
-        			line.setAttribute("y1", y1);
-        			line.setAttribute("x2", x2);
-        			line.setAttribute("y2", y2);
-        			line.setAttribute("style","stroke:rgb(255,0,0);stroke-width:2");
-        			var svg = document.getElementById("canvas");
-        			svg.insertBefore(line, svg.firstChild);
-        		}
-    		});
-		}*/
 		break;
 	
 		case "RemoveArcs":
 			var targetElement = event.target || event.srcElement;
+			//get Id's of the states the arc goes to and from
 			var toId = targetElement.parentNode.getAttribute("id");
 			var fromId = downTarget.parentNode.getAttribute("id");
 			//remove arc from list of to-State
@@ -384,7 +391,7 @@ function mouseUpHandler(e) {
 			if (indexFrom > -1){
 				arcListFrom.splice(indexFrom, 1);
 			}
-			//remove arc in display
+			//remove arc from document
 			removeArc = document.getElementById(fromId+toId);
 			try {
 				removeArc.remove();
@@ -393,11 +400,13 @@ function mouseUpHandler(e) {
 	}
 }
 
-
+//draw a self referential arc
 function drawArc(cx, cy, targetElement) {
+	// get Id of state on which to draw the arc
 	var toId = targetElement.parentNode.getAttribute("id");
 	var fromId = downTarget.parentNode.getAttribute("id");
-	var character = prompt("Please enter the character associated with this arc:")
+	var character = prompt("Please enter the character associated with this arc:");
+	//draw arc only if user enters some arc value
     if (character != null){
     	var toList = stateArcs[toId];							//add arc Id to stateArcs
 		toList[toList.length] = fromId+toId;
@@ -433,10 +442,12 @@ function drawArc(cx, cy, targetElement) {
 	}
 }
 
+//draws arc as a line between two states
 function drawLine(x1, y1, x2, y2, targetElement) {
 	var toId = targetElement.parentNode.getAttribute("id"); //find Ids of connected nodes
 	var fromId = downTarget.parentNode.getAttribute("id");
 	var character = prompt("Please enter the character associated with this arc:")
+	//only draw arc if user enters some arc value
     if (character != null){
 		var toList = stateArcs[toId];							//add arc Id to stateArcs
 		toList[toList.length] = fromId+toId;
@@ -447,6 +458,7 @@ function drawLine(x1, y1, x2, y2, targetElement) {
 		var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
 		var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
 		var dist = Math.sqrt(Math.pow(y1-y2, 2) + Math.pow(x1-x2, 2)); //determine coordinates of start and end of line
+		//calculate offset to make sure arrow is not hidden behind state
 		var xOffset = ((40*(x1-x2)) / dist);
 		var yOffset = ((40*(y1-y2)) / dist);
    		line.setAttribute("x1", this.x1);
@@ -473,6 +485,7 @@ function drawLine(x1, y1, x2, y2, targetElement) {
     }
 }
 
+//draw state at clicked coordinates
 function draw(e) {
     var pos = getMousePos(canvas, e);
     posx = pos.x;
@@ -498,7 +511,6 @@ function draw(e) {
 	stateText.setAttribute("fill", "#A9A9A9");
 	stateText.textContent = state;
 	g.appendChild(stateText);
-	//g.setAttribute("onclick", "clickHandler(this)");
 	var clickCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 	clickCircle.setAttribute("cx",posx);
 	clickCircle.setAttribute("cy",posy);
@@ -517,6 +529,7 @@ function draw(e) {
    
 }
 
+//get coordinates of mouse at event
 function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
